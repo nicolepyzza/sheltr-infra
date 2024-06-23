5d981451c3857c47e7e637ea6f644827ebcc8ce6
Nicole Pyzza <nicolepyzza@Nicoles-MacBook-Pro.local>
2024-06-23 08:52:23 -0400

    adding principals

--- s3.tf
+++ s3.tf
@@ -16,6 +16,11 @@ resource "aws_s3_bucket_policy" "sheltr_web" {
 
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
+    principals {
+      type = "*"
+      identifiers = [ "*" ]
+    }
+
     actions = [
       "s3:GetObject",
       "s3:ListBucket",
