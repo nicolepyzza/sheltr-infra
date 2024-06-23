3580721b3205528d42297dd8b10558bfa8f8d48b
Nicole Pyzza <nicolepyzza@Nicoles-MacBook-Pro.local>
2024-06-23 08:53:38 -0400

    fmt

--- s3.tf
+++ s3.tf
@@ -17,8 +17,8 @@ resource "aws_s3_bucket_policy" "sheltr_web" {
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     principals {
-      type = "*"
-      identifiers = [ "*" ]
+      type        = "*"
+      identifiers = ["*"]
     }
 
     actions = [
