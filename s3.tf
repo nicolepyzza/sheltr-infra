8e01e8089796a20eff61ca13fd76b78a9a4c3ff6
Nicole Pyzza <nicolepyzza@Nicoles-MacBook-Pro.local>
2024-07-19 15:42:59 -0400

    test

--- s3.tf
+++ s3.tf
@@ -9,10 +9,23 @@ resource "aws_s3_bucket_versioning" "sheltr_web" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "sheltr_web" {
-#   bucket = aws_s3_bucket.sheltr_web.id
-#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-# }
+resource "aws_s3_bucket_ownership_controls" "sheltr_web" {
+  bucket = aws_s3_bucket.sheltr_web.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.sheltr_web,
+    aws_s3_bucket_public_access_block.public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.sheltr_web.id
+  acl    = "public-read"
+}
 
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
@@ -41,7 +54,7 @@ resource "aws_s3_bucket_website_configuration" "sheltr_web" {
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket                  = aws_s3_bucket.sheltr_web.id
+  bucket = aws_s3_bucket.sheltr_web.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
